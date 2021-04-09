e244de272493153dcb14d37fdec58e209bf62f19
valkiar <77755040+valkiar@users.noreply.github.com>
2021-04-09 10:02:19 -0500

    Add files via upload

---
+++ recuperacion.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>prueba</title>
+<style type="funcion/css"
+ #tabla{
+ display:none;
+}
+</style>
+</style>
+<body bgcolor =lightpink >
+<center>
+	<button type="button"></button>
+<p>1</p>
+<div id="1">
+	<button type="button">
+<table  id="tabla" border="10px" width="1000px" height="1000px">
+	<h1>hola</h1>
+
+	<scrypt type="funcion/css">
+		function mostrartabla(){
+		document.getElemntyById("tabla")
+	}
+
+</body>
+
+</html>
